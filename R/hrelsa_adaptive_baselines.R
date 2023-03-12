d64581c4e1a889a8672b4f6fe1c1b7e81cdba0ce
P. Ohland <91417773+phohland@users.noreply.github.com>
2023-03-12 22:39:48 +0100

    Adaptive Baselines! v1

--- R/hrelsa_adaptive_baselines.R
+++ R/hrelsa_adaptive_baselines.R
@@ -12,11 +12,12 @@
 hrelsa_adaptive_baselines <-
   function(dat,
            vars = NULL,
+           turnvars = NULL,
            realtime = NULL,
 
            dob_dat = NULL,
            dob_data_id_col = NULL,
-           ddob_data_dob_col = NULL,
+           dob_data_dob_col = NULL,
 
            norm_dat = NULL,
            norm_dat_names = NULL
@@ -41,7 +42,7 @@ hrelsa_adaptive_baselines <-
       )
       abort <- TRUE
     }
-    if (length(vars) != length(norm_dat_names)) {
+    if (length(vars) != (length(norm_dat_names) - 1)) {
       warning(
         "The column names of the normal values need to have the same length.
         The baseline was not calculated."
@@ -49,7 +50,7 @@ hrelsa_adaptive_baselines <-
       abort <- TRUE
     }
 
-    # Function code -----------------------------------------------------------
+    # Normalization -----------------------------------------------------------
 
     dat <- as_tibble(dat)
     n   <- unique(dat$id)
@@ -63,6 +64,7 @@ hrelsa_adaptive_baselines <-
 
     dat$age <- 0
 
+    # Calculates the age regarding the dob of the extern table and the current time point
     for (l in 1:nrow(dat)) {
       timepoint <- dat$timepoint[l]
       dob <- dob_dat$dob[dob_dat$id == as.character(dat$id[l])]
@@ -75,16 +77,22 @@ hrelsa_adaptive_baselines <-
       norm            <- NULL
       for (i in 1:length(n)) {
 
-          # Here happens the normal variable normalization
-          mydayone      <- set[set$id == n[i], normthese[j]]
-          mydayone[,1]  <-
-            set[set$id == n[i] &
-                  set$time == ontime, normthese[j]]
-          mymeans       <-
-            (set[set$id %in% n[i], normthese[j]] / mydayone) * 100
+          norm_table      <- dat[dat$id == n[i], vars[j]]
+          age <- dat$age[dat$id == n[i]]
+          norm_table      <- cbind(norm_table, age)
 
-          mytime         <- set[set$id == n[i], "time"]
-          mytier        <- set[set$id == n[i], "id"]
+          for (l in 1:nrow(norm_table)) {
+            pat_age <- norm_table[l, 2]
+            target.index <- which(abs(norm_dat[, 1] - pat_age) == min(abs(norm_dat[, 1] - pat_age)))
+            norm_table[l, 1] <- norm_dat[target.index, j + 1]
+          }
+
+          # Here is vere the normalization with each base values happens
+          mymeans       <-
+            (dat[dat$id %in% n[i], normthese[j]] / norm_table[ , 1]) * 100
+
+          mytime         <- dat[dat$id == n[i], "time"]
+          mytier        <- dat[dat$id == n[i], "id"]
           norm          <-
             rbind(norm,
                   data.frame(
@@ -97,7 +105,7 @@ hrelsa_adaptive_baselines <-
           # (e.g., multiple measurements per time)
           n_occur       <- NULL
           n_occur       <-
-            data.frame(table(set[set$id %in% n[i], "time"]))
+            data.frame(table(dat[dat$id %in% n[i], "time"]))
           if (sum(n_occur$Freq) > length(n_occur[,1])) {
             warning(
               "There are multiple daily entries per id.
@@ -107,9 +115,48 @@ hrelsa_adaptive_baselines <-
 
       }
 
-      set[, which(names(set) == normthese[j])] <- norm[, 3]
+      dat[, which(names(dat) == normthese[j])] <- norm[, 3]
     }
 
 
-  return(raw)
+# Baseline ----------------------------------------------------------------
+
+      maxsev <- apply(dat[, vars], 2, min, na.rm = TRUE)
+
+    if (length(turnvars) > 0) {
+      maxsev[turnvars] <- apply(dat[, turnvars], 2, max, na.rm = TRUE)
+    }
+
+    # Fetch maximum delta between 100% and highest severity
+    maxdelta            <- abs(100 - maxsev)
+
+    # Model characteristics
+    ristics           <-
+      data.frame(
+        n          = length(unique(dat$id)),
+        treatments = length(unique(dat$treatment)),
+        conditions = length(unique(dat$condition)),
+        variables  = dim(dat[, vars])[2]
+      )
+
+    # Giving the baseline table, max severity, max delta and
+    # the characteristics out
+    # For just z variables without baseline table
+      ret <-
+        list(
+          pre = dat[ , -(which(names(dat) %in% c("age", "timepoint")))],
+          maxsev = maxsev,
+          maxdelta = maxdelta,
+          ristics = ristics
+        )
+
+    cat(
+      "The baseline has been calculated with",
+      length(vars),
+      "variables.",
+      length(turnvars),
+      "of them are turned variables."
+    )
+
+  return(ret)
 }
